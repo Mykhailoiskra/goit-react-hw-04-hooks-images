1b13a66870f954dc4e224114373b7b8c1fa21b3e
Mykhailo Iskra <iskramichel@gmail.com>
2020-12-26 13:19:02 +0200

    moves fetch logic to ImgGallery component

--- src/App.js
+++ src/App.js
@@ -1,88 +1,22 @@
 // Styles
 import "./App.css";
 import "react-toastify/dist/ReactToastify.css";
+
 // Global components
-import { useState, useEffect, useRef } from "react";
-import { createPortal } from "react-dom";
+import { useState } from "react";
 import { ToastContainer } from "react-toastify";
 
 // Local components
 import Searchbar from "./components/Searchbar";
 import ImageGallery from "./components/ImageGallery";
-import Modal from "./components/Modal";
-import API from "./services/pixabay-api";
-
-// Service variables
-const modalRoot = document.querySelector("#modal-root");
-const Status = {
-  IDLE: "idle",
-  PENDING: "pending",
-  RESOLVED: "resolved",
-  REJECTED: "rejected",
-};
 
 export default function App() {
   const [query, setQuery] = useState("");
   const [page, setPage] = useState(1);
-  const [pictures, setPictures] = useState([]);
-  const [errorMsg, setErrorMsg] = useState(null);
-  const [status, setStatus] = useState(Status.IDLE);
-  const [modal, setModal] = useState(false);
-  const [largeImg, setLargeImg] = useState({
-    url: "",
-    alt: "",
-  });
-
-  const isFirstRender = useRef(true);
-
-  useEffect(() => {
-    if (isFirstRender.current) {
-      isFirstRender.current = false;
-      return;
-    }
-    setStatus(Status.PENDING);
-    fetchPictures();
-  }, [query]);
-
-  useEffect(() => {
-    if (page === 1) {
-      return;
-    }
-
-    fetchPictures();
-  }, [page]);
-
-  const fetchPictures = () => {
-    API.fetchPictures(query, page)
-      .then((res) => {
-        setPictures((prevPictures) => [...prevPictures, ...res.hits]);
-        setStatus(Status.RESOLVED);
-        if (page !== 1) {
-          window.scrollTo({
-            top: document.documentElement.scrollHeight,
-            behavior: "smooth",
-          });
-        }
-      })
-      .catch((error) => {
-        setErrorMsg(error);
-        setStatus(Status.REJECTED);
-      });
-  };
 
   const handleSearchSubmit = (searchQuery) => {
     setQuery(searchQuery);
     setPage(1);
-    setPictures([]);
-  };
-
-  const handleImageClick = (imgSrc, alt) => {
-    setLargeImg({ url: imgSrc, alt });
-    toggleModal();
-  };
-
-  const toggleModal = () => {
-    setModal((modal) => !modal);
   };
 
   const handleLoadMore = () => {
@@ -92,19 +26,8 @@ export default function App() {
   return (
     <div className="container">
       <Searchbar onSubmit={handleSearchSubmit} />
-      <ImageGallery
-        pictures={pictures}
-        status={status}
-        error={errorMsg}
-        onClick={handleImageClick}
-        onLoadMore={handleLoadMore}
-      />
+      <ImageGallery onLoadMore={handleLoadMore} query={query} page={page} />
       <ToastContainer autoClose={3000} />
-      {modal &&
-        createPortal(
-          <Modal src={largeImg.url} alt={largeImg.alt} onClose={toggleModal} />,
-          modalRoot
-        )}
     </div>
   );
 }
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,8 +1,9 @@
 // Styles
 import "./ImageGalleryStyles.css";
 import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
-
+import { createPortal } from "react-dom";
 // Utilities components
+import { useState, useEffect, useRef } from "react";
 import { toast } from "react-toastify";
 import PropTypes from "prop-types";
 
@@ -10,18 +11,78 @@ import PropTypes from "prop-types";
 import Loader from "react-loader-spinner";
 import ImageGalleryItem from "../ImageGalleryItem";
 import Button from "../Button";
+import API from "../../services/pixabay-api";
+import Modal from "../Modal";
 
-export default function ImageGallery({
-  onClick,
-  pictures,
-  error,
-  status,
-  onLoadMore,
-}) {
-  const handleImageClick = (evt) => {
-    if (evt.target.tagName === "IMG") {
-      onClick(evt.target.dataset.url, evt.target.alt);
+const Status = {
+  IDLE: "idle",
+  PENDING: "pending",
+  RESOLVED: "resolved",
+  REJECTED: "rejected",
+};
+const modalRoot = document.querySelector("#modal-root");
+
+export default function ImageGallery({ onLoadMore, query, page }) {
+  const [errorMsg, setErrorMsg] = useState(null);
+  const [status, setStatus] = useState(Status.IDLE);
+  const [pictures, setPictures] = useState([]);
+  const [largeImg, setLargeImg] = useState({
+    url: "",
+    alt: "",
+  });
+  const [modal, setModal] = useState(false);
+
+  const isFirstRender = useRef(true);
+
+  useEffect(() => {
+    if (isFirstRender.current) {
+      isFirstRender.current = false;
+      return;
     }
+    setStatus(Status.PENDING);
+    fetchPictures();
+    // eslint-disable-next-line
+  }, [query]);
+
+  useEffect(() => {
+    if (page === 1) {
+      return;
+    }
+    fetchPictures();
+    // eslint-disable-next-line
+  }, [page]);
+
+  const fetchPictures = () => {
+    API.fetchPictures(query, page)
+      .then((res) => {
+        if (page === 1) {
+          setPictures(res.hits);
+        } else {
+          setPictures((prevPictures) => [...prevPictures, ...res.hits]);
+        }
+
+        setStatus(Status.RESOLVED);
+        if (page !== 1) {
+          window.scrollTo({
+            top: document.documentElement.scrollHeight,
+            behavior: "smooth",
+          });
+        }
+      })
+      .catch((error) => {
+        setErrorMsg(error);
+        setStatus(Status.REJECTED);
+      });
+  };
+
+  const handleImageClick = (event) => {
+    const imgSrc = event.target.dataset.url;
+    const alt = event.target.alt;
+    setLargeImg({ url: imgSrc, alt });
+    toggleModal();
+  };
+  const toggleModal = () => {
+    setModal((modal) => !modal);
   };
 
   if (status === "idle") {
@@ -40,10 +101,10 @@ export default function ImageGallery({
     );
   }
   if (status === "rejected") {
-    return toast.error(error);
+    return toast.error(errorMsg);
   }
 
-  if (status === "resolved" && pictures.length !== 0) {
+  if (status === "resolved") {
     return (
       <>
         <ul className="ImageGallery" onClick={handleImageClick}>
@@ -57,6 +118,15 @@ export default function ImageGallery({
           ))}
         </ul>
         <Button onClick={onLoadMore} />
+        {modal &&
+          createPortal(
+            <Modal
+              src={largeImg.url}
+              alt={largeImg.alt}
+              onClose={toggleModal}
+            />,
+            modalRoot
+          )}
       </>
     );
   } else {
